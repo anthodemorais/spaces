2c39ae3e5fd6065e49ba7acb0d754009b5f91521
Anthony <anthonyd-m@live.com>
2019-10-29 12:11:59 +0100

    finished

--- spaces.py
+++ spaces.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--path", help="the path to replace in", required=True)
+parser = argparse.ArgumentParser(description="Searches and replaces in filenames.")
+parser.add_argument("path", help="the path to replace in")
 parser.add_argument("--recurse", help="replaces spaces with underscores in the subdirectories filenames too", required=False, action="store_true")
 parser.add_argument("--search", help="the character to look for, default is ' '", required=False, default=" ")
 parser.add_argument("--replace", help="the character to to replace with; default is '_'", required=False, default="_")
@@ -12,7 +12,6 @@ pattern = "*"
 if args.recurse is not None:
     pattern = "**/*"
 
-print(args.search, args.replace)
 for child in list(Path(args.path).glob(pattern)):
     name = child.name.replace(args.search, args.replace)
-    child.rename(name)
+    child.rename(child.parent / name)
